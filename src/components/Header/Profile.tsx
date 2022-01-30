ed1b47e9898a398bf4d009363e521f1aa8e29707
Elias Junior <elijah.junnior@outlook.com>
2022-01-30 14:57:24 -0300

    testing responsive components

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileDate?: boolean
+}
+
+export function Profile({ showProfileDate = true }: ProfileProps) {
     return (
         <Flex align='center'>
-            <Box mr='4' textAlign='right'>
-                <Text>Elias Jr</Text>
-                <Text color='gray.300' fontSize='small'>
-                    elias@go2sistemas.com.br
-                </Text>
-            </Box>
+            {showProfileDate && (
+                <Box mr='4' textAlign='right'>
+                    <Text>Elias Jr</Text>
+                    <Text color='gray.300' fontSize='small'>
+                        elias@go2sistemas.com.br
+                    </Text>
+                </Box>
+            )}
             <Avatar size='md' name='Elias Jr' />
         </Flex>
     )
