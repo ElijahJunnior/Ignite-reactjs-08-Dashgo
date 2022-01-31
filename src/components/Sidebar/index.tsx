43da6965062f8a62a0d8a0c5ea66e15393f52117
Elias Junior <elijah.junnior@outlook.com>
2022-01-31 06:55:48 -0300

    Component SidebarDrawer created to turn the Sidebar responsive

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,21 +1,36 @@
-import { Box, Stack } from '@chakra-ui/react';
-import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from 'react-icons/ri'
-import { NavLink } from './NavLink';
-import { NavSection } from './NavSection';
+import { Box, DrawerBody, DrawerHeader, useBreakpointValue } from '@chakra-ui/react';
+import { Drawer, DrawerCloseButton, DrawerContent, DrawerOverlay } from '@chakra-ui/react';
+import { useSidebarDrawer } from '../../context/SidebarDrawerContext';
+import { SidebarNav } from './SidebarNav';
 
 export function Siderbar() {
+
+    const { isOpen, onClose } = useSidebarDrawer()
+
+    const isDrawerSidebar = useBreakpointValue({
+        base: true,
+        lg: false
+    })
+
     return (
-        <Box as='aside' w='64' mr='8'>
-            <Stack spacing='12' align='flex-start' >
-                <NavSection title='GERAL'>
-                    <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-                    <NavLink icon={RiContactsLine}>Usuários</NavLink>
-                </NavSection>
-                <NavSection title='AUTOMAÇÃO'>
-                    <NavLink icon={RiInputMethodLine}>Formulário</NavLink>
-                    <NavLink icon={RiGitMergeLine}>Automação</NavLink>
-                </NavSection>
-            </Stack>
-        </Box>
+
+        (isDrawerSidebar) ? (
+            <Drawer isOpen={isOpen} placement='left' onClose={onClose} >
+                <DrawerOverlay>
+                    <DrawerContent bg='gray.800' p='4'>
+                        <DrawerCloseButton mt='6' />
+                        <DrawerHeader>Navegação</DrawerHeader>
+                        <DrawerBody>
+                            <SidebarNav />
+                        </DrawerBody>
+                    </DrawerContent>
+                </DrawerOverlay>
+            </Drawer >
+        ) : (
+            <Box as='aside' w='64' mr='8'>
+                <SidebarNav />
+            </Box>
+        )
     )
+
 }
