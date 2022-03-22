7919c0e132082c867a45fe0df9160d87112d1cdd
Elias Junior <elijah.junnior@outlook.com>
2022-03-22 13:26:28 -0300

    developing fake api

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,7 +1,7 @@
 import { createServer, Factory, Model } from 'miragejs';
 import faker from 'faker';
 
-type User = {
+type Costumer = {
     name: string;
     email: string;
     created_at: string;
@@ -9,41 +9,89 @@ type User = {
 
 export function makeServer() {
     const server = createServer({
-        // informa o modelo das entidades usadas nas rotas
         models: {
-            user: Model.extend<Partial<User>>({})
+            costumer: Model.extend<Partial<Costumer>>({})
         },
-        // cria dados iniciais em massa para serem usados nas rotas
         factories: {
-            user: Factory.extend({
-                // usa o indice para criar diversos usuarios numerados
-                name(i: number) {
-                    return `User ${i + 1}`;
+            costumer: Factory.extend({
+                name() {
+                    // usa a bibliotec faker para gerar o nome de usuario
+                    // return faker.name.findName();
+                    return 'elias'
                 },
                 email() {
-                    return faker.internet.email().toLowerCase();
+                    // usa a bibliotec faker para gerar o email do usuario
+                    // return faker.internet.email().toLowerCase();
+                    return 'elias@email.com'
                 },
                 // ter definido o modelo com snake case e o 
                 // factory com camel case não vai gerar problemas
                 createdAt() {
-                    return faker.date.recent(10);
+                    return '03-22-2022'
+                    // usa a bibliotec faker para gerar a data de criação do usuario
+                    // return faker.date.recent(10);
                 }
             })
         },
+        seeds(server) {
+            server.createList("costumer", 200)
+        },
         routes() {
-            // raiz da url do mirage ex.: (localhost:3001/api/users)
             this.namespace = 'api';
-            // Atrasa em x milesegundos a resposta da api 
-            this.timing = 750;
-            // CRUD de User
-            this.get('/users');
-            this.post('/users');
-            this.put('/users');
-            this.delete('/users');
-            // resetando a name space para não interferir nas API do Next
-            this.namespace = 'api';
-            // encaminha as rotas que não foram encontradas para a API do Next
+            this.get('/costumers')
+            this.namespace = '';
             this.passthrough();
         }
     })
+
+    // export function makeServer() {
+    //     const server = createServer({
+    //         // informa o modelo das entidades usadas nas rotas
+    //         models: {
+    //             costumer: Model.extend<Partial<Costumer>>({})
+    //         },
+    //         // cria dados iniciais em massa para serem usados nas rotas
+    //         factories: {
+    //             costumer: Factory.extend({
+    //                 name() {
+    //                     // usa a bibliotec faker para gerar o nome de usuario
+    //                     // return faker.name.findName();
+    //                     return 'elias'
+    //                 },
+    //                 email() {
+    //                     // usa a bibliotec faker para gerar o email do usuario
+    //                     // return faker.internet.email().toLowerCase();
+    //                     return 'elias@email.com'
+    //                 },
+    //                 // ter definido o modelo com snake case e o 
+    //                 // factory com camel case não vai gerar problemas
+    //                 createdAt() {
+    //                     return '03-22-2022'
+    //                     // usa a bibliotec faker para gerar a data de criação do usuario
+    //                     // return faker.date.recent(10);
+    //                 }
+    //             })
+    //         },
+    //         seeds(server) {
+    //             server.createList("costumer", 200)
+    //         },
+    //         routes() {
+    //             // raiz da url do mirage ex.: (localhost:3001/api/users)
+    //             this.namespace = '';
+    //             // Atrasa em x milesegundos a resposta da api 
+    //             this.timing = 750;
+    //             // CRUD de User
+    //             this.get('/costumer');
+    //             // this.post('/users');
+    //             // this.put('/users');
+    //             // this.delete('/users');
+    //             // resetando a name space para não interferir nas API do Next
+    //             // this.namespace = '';
+    //             // encaminha as rotas que não foram encontradas para a API do Next
+    //             // this.passthrough();
+    //         }
+    //     })
+
+
+    return server
 }
