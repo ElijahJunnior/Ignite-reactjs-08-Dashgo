feb5a20715ec30cf6c77de71b4afaa8cbc256489
Elias Junior <elijah.junnior@outlook.com>
2022-03-30 07:12:52 -0300

    adding pagination resource into consume to get users

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from 'miragejs';
+import { createServer, Factory, Model, Response } from 'miragejs';
 import faker from 'faker';
 
 type User = {
@@ -32,15 +32,39 @@ export function makeServer() {
         },
         seeds(server) {
             // configura o mirage para criar 200 registros de user quando for iniciado
-            server.createList("user", 10)
+            server.createList("user", 200)
         },
         routes() {
             // raiz da url do mirage ex.: (localhost:3001/api/users)
             this.namespace = 'api';
             // Atrasa em x milesegundos a resposta da api 
             this.timing = 750;
-            // CRUD de User
-            this.get('/users')
+            // CRUD de User - Sobrecarregado para suportar paginação
+            this.get('/users', function (schema, request) {
+                // pega os parametros passados via query 
+                const { page = 1, per_page = 10 } = request.queryParams;
+                // pega a quantidade total de registros da entidade do consumo
+                const total = schema.all('user').length;
+                // calcula o primeiro registro 
+                const pageStart = (Number(page) - 1) * Number(per_page);
+                // calcula o ultimo registro
+                const pageEnd = pageStart + Number(per_page);
+                // pega a lista do usuarios do mirage e corta ela para fazer a paginação
+                const users = this.serialize(schema.all('user'))
+                    .users.slice(pageStart, pageEnd);
+                // retorna o resultado da consulta 
+                return new Response(
+                    // codigo de status da cunsulta 
+                    200,
+                    // adiciona headers a resposta
+                    // por boa pratica deve se adicionar oque não é o objetivo da consulta por header
+                    // informa ao usuario a quantidade total de registros
+                    { 'x-total-count': String(total) },
+                    // objeto retornado no body da consulta 
+                    users
+                )
+
+            })
             this.post('/users');
             this.put('/users');
             this.delete('/users');
